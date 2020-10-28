916df69036cacf79fd429e213188c9b09be1cc94
Keith Hair <fabrocat.connect@gmail.com>
2020-10-28 01:30:38 -0400

    For searching Information_Schema in database

---
+++ SEARCH_IN_SCHEMA.sql
@@ -0,0 +1,21 @@
+-- Each given variable will narrow the results.
+DECLARE @FindColumnNameLike varchar(70)=''					
+DECLARE @FindTableNameLike varchar(70)=''
+DECLARE @FindDataType varchar(70)=''
+DECLARE @FindSchemaName varchar(70)=''
+
+SELECT
+	TABLE_CATALOG+'.'+TABLE_SCHEMA+'.'+	TABLE_NAME	AS 'TableName',
+	COLUMN_NAME										AS 'ColumnName',
+	DATA_TYPE										AS 'DataType',
+	CHARACTER_MAXIMUM_LENGTH						AS 'MaxCharLengh',
+	IS_NULLABLE										AS 'IsNullable'
+FROM INFORMATION_SCHEMA.COLUMNS c
+WHERE
+	(c.COLUMN_NAME LIKE '%'+@FindColumnNameLike+'%')
+	AND
+	(c.TABLE_NAME LIKE '%'+@FindTableNameLike+'%')
+	AND
+	(c.DATA_TYPE = @FindDataType OR @FindDataType='')
+	AND
+	(c.TABLE_SCHEMA = @FindSchemaName OR @FindSchemaName='')
