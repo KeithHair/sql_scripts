5b83d979548bbecd01bb8e3d4da010eef8914a74
Keith Hair <fabrocat.connect@gmail.com>
2020-10-29 01:45:06 -0400

    Shows Constraint values  only for column with the constraint

--- SEARCH_IN_SCHEMA.sql
+++ SEARCH_IN_SCHEMA.sql
@@ -6,7 +6,7 @@ DECLARE @FindSchemaName varchar(70)=''
 DECLARE @MinCharLen int=0
 DECLARE @MaxCharLen int=0
 DECLARE @IsNullable varchar(3)='' -- yes or no
-DECLARE @ShowConstraints varchar(3)='' -- yes or no
+DECLARE @ShowConstraints varchar(3)='yes' -- yes or no
 DECLARE @FindConstraintLike varchar(70)='' -- @ShowContraint must be 'yes'
 
 IF OBJECT_ID('tempdb.dbo.#tmp1','U') IS NOT NULL
@@ -18,12 +18,15 @@ SELECT
 	c.DATA_TYPE													AS 'DataType',
 	c.CHARACTER_MAXIMUM_LENGTH									AS 'MaxCharLength',
 	c.IS_NULLABLE												AS 'IsNullable',
-	CASE WHEN @ShowConstraints = 'yes' THEN	u.CONSTRAINT_SCHEMA+'.'+u.CONSTRAINT_NAME
+	CASE WHEN @ShowConstraints = 'yes' THEN
+		(select top 1 u.CONSTRAINT_NAME from INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE u
+		where (u.TABLE_CATALOG+'.'+u.TABLE_SCHEMA+'.'+u.TABLE_NAME+'.'+c.COLUMN_NAME) = (c.TABLE_CATALOG+'.'+c.TABLE_SCHEMA+'.'+c.TABLE_NAME+'.'+u.COLUMN_NAME)
+		and	(u.CONSTRAINT_NAME LIKE '%'+@FindConstraintLike+'%'))
 	ELSE '----'
-	END AS 'ConstraintName'
+	END															AS 'ConstraintName'
+
 FROM INFORMATION_SCHEMA.COLUMNS c
-JOIN INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE u
-ON (c.TABLE_CATALOG+'.'+c.TABLE_SCHEMA+'.'+c.TABLE_NAME) = (u.TABLE_CATALOG+'.'+u.TABLE_SCHEMA+'.'+u.TABLE_NAME)
+
 
 WHERE
 	(c.COLUMN_NAME LIKE '%'+@FindColumnNameLike+'%')
@@ -38,5 +41,5 @@ WHERE
 	  OR (@MinCharLen=0 AND @MaxCharLen=0))
 	AND
 	(c.IS_NULLABLE = @IsNullable OR @IsNullable='')
-	AND
-	(u.CONSTRAINT_NAME LIKE '%'+@FindConstraintLike+'%')
+
+
